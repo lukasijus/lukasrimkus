5d97c6158945a05e972a609f315a698f7613c5ac
Lukasijus <lukasrimkuslg@Gmail.com>
2019-12-08 00:13:21 +0100

    comment defaultDataLayer: { platform: gatsby },

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,12 +40,12 @@ module.exports = {
   
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: true,
+        includeInDevelopment: false,
   
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
+        // defaultDataLayer: { platform: "gatsby" },
   
         // Specify optional GTM environment details.
         //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
